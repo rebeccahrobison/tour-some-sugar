8ba6bd3b9a92650751dbe21f977097a323bf4536
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-23 15:25:13 -0500

    Add bandMembers array, populate objects, export

--- scripts/database.js
+++ scripts/database.js
@@ -1,32 +1,38 @@
 const database = {
     bands: [{
+        id: 1,
         name: "Scheherazade",
         members: 3,
         genre: "Classical Punk",
         yearEst: 1990
     }, {
+        id: 2,
         name: "Bananas in Pajamas",
-        members: 5,
+        members: 2,
         genre: "Pop",
         yearEst: 2001
     }, {
+        id: 3,
         name: "Trouble with Tribbles",
         members: 2,
         genre: "Space LoFi",
         yearEst: 2005
     }, {
+        id: 4,
         name: "Monkey House",
         members: 4,
         genre: "Alternative",
         yearEst: 1999
     }, {
+        id: 5,
         name: "Sherlock",
         members: 4,
         genre: "Dubstep",
         yearEst: 2010
     }, {
+        id: 6,
         name: "Sequence Diagram",
-        members: 26,
+        members: 1,
         genre: "Esoterica",
         yearEst: 2000
     }],
@@ -91,6 +97,119 @@ const database = {
         bandName: "Sequence Diagram",
         venueName: "Thunderdome",
         date: "8/27/2023"     
+    }],
+    bandMembers: [{
+        id: 1,
+        firstName: "A",
+        lastName: "B",
+        yearBorn: 1980,
+        role: "lead singer",
+        bandId: 1
+    }, {
+        id: 2,
+        firstName: "C",
+        lastName: "D",
+        yearBorn: 1981,
+        role: "guitar",
+        bandId: 1
+    }, {
+        id: 3,
+        firstName: "E",
+        lastName: "F",
+        yearBorn: 1982,
+        role: "piano",
+        bandId: 1
+    }, {
+        id: 4,
+        firstName: "G",
+        lastName: "H",
+        yearBorn: 1983,
+        role: "glockenspiel",
+        bandId: 2
+    }, {
+        id: 5,
+        firstName: "I",
+        lastName: "J",
+        yearBorn: 1984,
+        role: "triangle",
+        bandId: 2
+    }, {
+        id: 6,
+        firstName: "K",
+        lastName: "L",
+        yearBorn: 1985,
+        role: "theramin",
+        bandId: 3
+    }, {
+        id: 7,
+        firstName: "M",
+        lastName: "N",
+        yearBorn: 1986,
+        role: "lead singer",
+        bandId: 3
+    }, {
+        id: 8,
+        firstName: "O",
+        lastName: "P",
+        yearBorn: 1987,
+        role: "bass guitar",
+        bandId: 4
+    }, {
+        id: 9,
+        firstName: "Q",
+        lastName: "R",
+        yearBorn: 1988,
+        role: "lead singer",
+        bandId: 4
+    }, {
+        id: 10,
+        firstName: "S",
+        lastName: "T",
+        yearBorn: 1989,
+        role: "guitar",
+        bandId: 4
+    }, {
+        id: 11,
+        firstName: "U",
+        lastName: "V",
+        yearBorn: 1990,
+        role: "drummer",
+        bandId: 4
+    }, {
+        id: 12,
+        firstName: "W",
+        lastName: "X",
+        yearBorn: 1991,
+        role: "lead singer",
+        bandId: 5
+    }, {
+        id: 13,
+        firstName: "Y",
+        lastName: "Z",
+        yearBorn: 1992,
+        role: "keyboard",
+        bandId: 5
+    }, {
+        id: 14,
+        firstName: "Aa",
+        lastName: "Bb",
+        yearBorn: 1993,
+        role: "guitar",
+        bandId: 5
+    }, {
+        id: 15,
+        firstName: "Cc",
+        lastName: "Dd",
+        yearBorn: 1994,
+        role: "bass guitar",
+        bandId: 5
+    }, {
+        id: 16,
+        firstName: "Ee",
+        lastName: "Ff",
+        yearBorn: 1995,
+        role: "penny whistle",
+        bandId: 6
     }]
 }
 
@@ -101,6 +220,11 @@ export const getBands = () => {
 export const getVenues = () => {
     return database.venues.map(venue => ({...venue}))
 }
+
 export const getBookings = () => {
     return database.bookings.map(booking => ({...booking}))
+}
+
+export const getBandMembers = () => {
+    return database.bandMembers.map(bandMember => ({...bandMember}))
 }
