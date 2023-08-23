1ae3664a33e501d1dca5f77d194e68a18034ade7
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-23 15:25:50 -0500

    Import getBandMembers, Add band member info to window alert

--- scripts/bands.js
+++ scripts/bands.js
@@ -1,13 +1,31 @@
-import { getBands, getBookings, getVenues } from "./database.js"
+import { getBands, getBookings, getBandMembers } from "./database.js"
 
 const bands = getBands()
 const bookings = getBookings()
+const bandMembers = getBandMembers()
 
 document.addEventListener(
     "click",
     (clickEvent) => {
         const clickedItem = clickEvent.target
         if (clickedItem.dataset.type === "band") {
+            let alertText = ``
+            let bandMembersArray = []
+            
+            for (const bandMember of bandMembers) {
+                console.log(clickedItem.dataset.id)
+                console.log(bandMember.bandId)
+                if (parseInt(clickedItem.dataset.id) === bandMember.bandId) {
+                    bandMembersArray.push(bandMember)
+                    console.log(bandMembersArray)
+                }
+
+            }
+            for (const bandMember of bandMembersArray) {
+                alertText += `
+                ${bandMember.firstName} ${bandMember.lastName} (${bandMember.role})`
+            }
+
             let venuesArray = []
             let venuesCounter = 0
             
@@ -19,18 +37,29 @@ document.addEventListener(
                 }
             }
             
+
+
             if (venuesCounter === 0) {
-                window.alert(`${clickedItem.dataset.name} isn't playing at any venues`)
+                alertText += `No upcoming shows`
+                window.alert(alertText)
             } else if (venuesCounter === 1) {
-                window.alert(`${clickedItem.dataset.name} is booked at ${venuesArray[0]}`)
+                alertText += `
+                
+                Upcoming shows:
+                ${venuesArray[0]}`
+                window.alert(alertText)
             } else {
-                let alertText = `${clickedItem.dataset.name} is booked at `
+                alertText += `
+                
+                Upcoming Shows:`
                 let i = 0
                 while (i < venuesArray.length - 1) {
-                    alertText += `${venuesArray[i]}, `
+                    alertText += `
+                ${venuesArray[i]}, `
                     i++
                 }
-                alertText += `${venuesArray[i]}`
+                alertText += `
+                ${venuesArray[i]}`
                 window.alert(alertText)
             }
         }
@@ -44,6 +73,7 @@ export const Bands = () => {
     for (const band of bands) {
         html += `<li
                 data-type="band"
+                data-id="${band.id}"
                 data-name="${band.name}"
                 data-genre="${band.genre}"
                 data-year="${band.yearEst}"
